85f5c926ed52b56f4f7a95ed14f875c07302ad69
Carlos López Calvo <clopezcalvo@cifpfbmoll.eu>
2024-10-08 11:42:41 +0200

    Update Piedra_papel_tijeras.java

--- Piedra_papel_tijeras.java
+++ Piedra_papel_tijeras.java
@@ -1,7 +1,7 @@
 //Esta es mi version del piedra papel tijera, lo he hecho usando variables de tipo String.
-Para poder jugar primero tendriamos que declarar que queremos que saque el jugador P1 y el P2, en este caso P1 sacara piedra y P2 sacara tijeras.
-A continuacion iran una serie de condicionales estos estan estructurados de la siguiente manera.
-Si(if) (P1 es igual(==) a piedra y(&&) P2 es igual(==) a papel) gana P2, y asi se repite con cada posibilidad del juego. 
+//Para poder jugar primero tendriamos que declarar que queremos que saque el jugador P1 y el P2, en este caso P1 sacara piedra y P2 sacara tijeras.
+//A continuacion iran una serie de condicionales estos estan estructurados de la siguiente manera.
+//Si(if) (P1 es igual(==) a piedra y(&&) P2 es igual(==) a papel) gana P2, y asi se repite con cada posibilidad del juego. 
 //
 public class MainClass {
   public static void main(String[] args) {
