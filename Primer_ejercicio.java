ecf327d52ac0e5451d65ca30397ee2cabc6a1198
Carlos López Calvo <clopezcalvo@cifpfbmoll.eu>
2024-10-08 11:41:06 +0200

    Update and rename Primer ejercicio to Primer_ejercicio.java

--- Primer_ejercicio.java
+++ Primer_ejercicio.java
@@ -1,4 +1,4 @@
-Para este ejercicio he creado una variable en este caso suma que es equivalente a la suma de 5, 4, 3, 2, 1.
+//Para este ejercicio he creado una variable en este caso suma que es equivalente a la suma de 5, 4, 3, 2, 1.//
 
 public class MainClass {
   public static void main(String[] args) {
